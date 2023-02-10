d8133808cd1d8e6203e488f9814db3e185124d5e
Ivan Makarov <I.Makarov@student.tudelft.nl>
2023-02-10 17:10:27 +0100

    Added k-fold cross-validation

--- PseudoLabels.py
+++ PseudoLabels.py
@@ -16,74 +16,60 @@ from sklearn.semi_supervised import LabelSpreading
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 
-classifiers = {"KNN": KNeighborsClassifier(), "MLP": MLPClassifier(), "SVC": SVC(), 
+
+models = {"KNN": KNeighborsClassifier(), "MLP": MLPClassifier(), "SVC": SVC(), 
                "Desicion Tree":  DecisionTreeClassifier(), "Random Forest Classifier": RandomForestClassifier(),
                "Gaussian":  GaussianNB(), "QDA": QuadraticDiscriminantAnalysis(), 
-               "Logistic Regression": LogisticRegression()}
+               "Logistic Regression": LogisticRegression(), "Label Propagation": LabelPropagation(kernel='knn'),
+               "Label Spreading": LabelSpreading(kernel='knn')}
 
 class PseudoLabels:
     def __init__(self, X, y, num_labeled):
         self.X_labeled = X[:num_labeled]
         self.y_labeled = y[:num_labeled]
 
-        self.X_unlabeled = X[num_labeled:]   
+        self.X_unlabeled = X[num_labeled:]  
+        
+        self.k_fold = KFold(n_splits=10)
         
     
-    def label_propagation(self):
-        model = LabelPropagation(kernel='knn')
-
-        model.fit(self.X_labeled, self.y_labeled)
-        #print("Accuracy (without pseudo-labels): ",  model.score(X_test, y_test))
-        
-        pseudo_labels = model.predict(self.X_unlabeled)
-            
-        X_combined = np.concatenate((self.X_labeled, self.X_unlabeled))
-        y_combined = np.concatenate((self.y_labeled, pseudo_labels))
-
-        model.fit(X_combined, y_combined)
-
-        #accuracy = model.score(X_test, y_test)
-        #print('Accuracy:', model.score(X_test, y_test))
-    
-    def label_spreading(self):
-        pass
-    
-    def self_training(self, model_name, model):
-        models_statistics = []
-        
-        kfold = KFold(n_splits=10)
-        
+    def k_fold_cross_validation(self, model, X, Y):
         scores = []
-        for train_index, val_index in kfold.split(self.X_labeled):
-            X_train_fold, X_val = self.X_labeled[train_index], self.X_labeled[val_index]
-            y_train_fold, y_val = self.y_labeled[train_index], self.y_labeled[val_index]
+        for train_index, test_index in self.k_fold.split(X):
+            X_train_fold, X_test_fold = X[train_index], X[test_index]
+            y_train_fold, y_test_fold = y[train_index], y[test_index]
             
             model.fit(X_train_fold, y_train_fold)
-            score = model.score(X_val, y_val)
+            score = model.score(X_test_fold, y_test_fold)
             scores.append(score)
+            
+        accuracy = np.mean(scores)
+        return accuracy
         
-        models_statistics.append([model_name, model, np.mean(scores)])
+    
+    def train_supervised_model(self, model_name):
+        model = models[model_name]
         
-        pseudo_labels = model.predict(self.X_unlabeled)
+        accuracy = self.k_fold_cross_validation(model, self.X_labeled, self.y_labeled)
+        
+        return {"model_name": model_name, "model": model, 
+                "accuracy": accuracy, "pseudo_labels": False}
+    
+    
+    def train_semi_supervised_model(self, model_name, supervised_model):
+        model = models[model_name]
+        
+        pseudo_labels = supervised_model.predict(self.X_unlabeled)
             
         # Combine the labeled and pseudo labeled data
         X_combined = np.concatenate((self.X_labeled, self.X_unlabeled))
         y_combined = np.concatenate((self.y_labeled, pseudo_labels))
-
-        model.fit(X_combined, y_combined)
         
-        scores = []
-        for train_index, val_index in kfold.split(X_combined):
-            X_train_fold, X_val = X_combined[train_index], X_combined[val_index]
-            y_train_fold, y_val = y_combined[train_index], y_combined[val_index]
-            
-            model.fit(X_train_fold, y_train_fold)
-            score = model.score(X_val, y_val)
-            scores.append(score)
+        accuracy = self.k_fold_cross_validation(model, X_combined, y_combined)
         
-        models_statistics.append([model_name + "(with pseudo-labels)", model, np.mean(scores)])
         
-        return models_statistics
+        return {"model_name": model_name, "model": model, 
+                "accuracy": accuracy, "pseudo_labels": True}
       
     
 data = pd.read_csv("data.csv")
@@ -94,13 +80,15 @@ y = data.iloc[:, -1].values
 X = np.array(X)
 y = np.array(y)
 
-pseudo_labels = PseudoLabels(X, y, 100)
-pseudo_labels.label_propagation()
+pseudo_labels = PseudoLabels(X, y, 300)
 
 models_statistics = []
 
-for clf_name in classifiers:
-    for model_stat in pseudo_labels.self_training(clf_name, classifiers[clf_name]):
-        models_statistics.append(model_stat)
+for model_name in models:
+    supervised_model_overview = pseudo_labels.train_supervised_model(model_name)
+    models_statistics.append(supervised_model_overview)
+    
+    semi_supervised_model_overview = pseudo_labels.train_semi_supervised_model(model_name, supervised_model_overview["model"])
+    models_statistics.append(semi_supervised_model_overview)
         
 print(models_statistics)
